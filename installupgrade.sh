fe9ff1305946d6e7ee73ac0c8ac8f7183c183df0
Unimat45 <73135178+Unimat45@users.noreply.github.com>
2020-11-08 20:21:01 -0500

    Update installupgrade.sh

--- installupgrade.sh
+++ installupgrade.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget -O ~/update.sh https://raw.githubusercontent.com/Unimat45/UbuntuUpdate/main/update.sh
-wget -O ~/upgrade.sh https://raw.githubusercontent.com/Unimat45/UbuntuUpdate/main/upgrade.sh
+wget -qO ~/update.sh https://raw.githubusercontent.com/Unimat45/UbuntuUpdate/main/update.sh
+wget -qO ~/upgrade.sh https://raw.githubusercontent.com/Unimat45/UbuntuUpdate/main/upgrade.sh
 chmod 755 ~/update.sh ~/upgrade.sh
